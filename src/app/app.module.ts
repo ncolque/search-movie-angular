8ca99da1be86c7649d33dfe3a990798b83e75efc
Nicolas Colque <nicolascon93@gmail.com>
2023-02-03 00:10:25 -0400

    Plantilla de la app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { CardMovieComponent } from './components/card-movie/card-movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    CardMovieComponent
   ],
   imports: [
     BrowserModule,
